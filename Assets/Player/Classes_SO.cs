82a7ebe6f84972c9262c957fdd7c097ad156849f
Geert Tullmans <519753@student.glu.nl>
2018-02-08 12:03:24 +0100

    Initialize

---
+++ Assets/Player/Classes_SO.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes_SO : ScriptableObject {
+
+    [SerializeField]
+    private int _Strength;
+    public int Strength
+    {
+        get { return _Strength; }
+    }
+    [SerializeField]
+    private int _Constitution;
+    public int Constitution
+    {
+        get { return _Constitution; }
+    }
+    [SerializeField]
+    private int _Dexterity;
+    public int Dexterity
+    {
+        get { return _Dexterity; }
+    }
+    [SerializeField]
+    private int _Agilety;
+    public int Agilety
+    {
+        get { return _Agilety; }
+    }
+    [SerializeField]
+    private int _Intelligence;
+    public int Intelligence
+    {
+        get { return _Intelligence; }
+    }
+    [SerializeField]
+    private int _Wisdom;
+    public int Wisdom
+    {
+       get { return _Wisdom; }
+    }
+}
---
+++ Assets/Player/PlayerStats.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+    [SerializeField]
+    private int _Strength;
+    [SerializeField]
+    private int _Constitution;
+    [SerializeField]
+    private int _Dexterity;
+    [SerializeField]
+    private int _Agilety;
+    [SerializeField]
+    private int _Intelligence;
+    [SerializeField]
+    private int _Wisdom;
+
+    public void SetClass(Classes_SO Class)
+    {
+        _Strength     = Class.Strength;
+        _Constitution = Class.Constitution;
+        _Dexterity    = Class.Dexterity;
+        _Agilety      = Class.Agilety;
+        _Intelligence = Class.Intelligence;
+        _Wisdom       = Class.Wisdom;
+
+    }
+
+    public int GSStrength
+    {          
+        get { return _Strength; }
+        set { _Strength = value; }
+    }          
+               
+    public int GSConstitution
+    {
+        get { return _Constitution; }
+        set { _Constitution = value; }
+    }
+   
+    public int GSDexterity
+    {
+        get { return _Dexterity; }
+        set { _Dexterity = value; }
+    }
+    
+    public int GSAgilety
+    {
+        get { return _Agilety; }
+        set { _Agilety = value; }
+    }
+    
+    public int GSIntelligence
+    {
+        get { return _Intelligence; }
+        set { _Intelligence = value; }
+    }
+    
+    public int GSWisdom
+    {
+        get { return _Wisdom; }
+        set { _Wisdom = value; }
+    }
+}
