c9847f7e4620438264bb3fd3f423d189744c53cb
Unknown <zdanylenko@outlook.com>
2018-04-20 14:33:31 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Cards/crds/Cardbuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Cardbuild")]
+public class Cardbuilder : ScriptableObject {
+
+    [SerializeField]
+    private string cardname; public string _cardname { get { return (cardname); } set { cardname = value; } }
+    [SerializeField]
+    private Sprite cardsprite; public Sprite _cardsprite { get { return (cardsprite); } set { cardsprite = value; } }
+    [SerializeField]
+    private int cardclass; public int _cardclass { get { return (cardclass); } set { cardclass = value; } }
+    [SerializeField]
+    private int cardtype; public int _cardtype { get { return (cardtype); } set { cardtype = value; } }
+    [SerializeField]
+    private int cardcost; public int _cardcost { get { return (cardcost); } set { cardcost = value; } }
+    [SerializeField]
+    private string cardtext; public string _cardtext { get { return (cardtext); } set { cardtext = value; } }
+    [SerializeField]
+    private float[] cardscaling; public float[] _cardscaling { get { return (cardscaling); } set { cardscaling = value; } } 
+}
