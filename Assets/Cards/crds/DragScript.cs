c07615bc4d8c382d45f030589f3080e6e8ea0450
Geert Tullmans <519753@student.glu.nl>
2018-05-25 13:25:19 +0200

    drag and drop toegevoegt

---
+++ Assets/Cards/crds/DragScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour {
+    private Vector3 MousePos;
+    private bool Kliked;
+    private Collider CardCollider;
+    // Use this for initialization
+    void Start () {
+        Kliked = false;
+        CardCollider = gameObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+
+        MousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            { 
+                
+                if (hit.collider == CardCollider)
+                {
+
+
+                    if (Kliked)
+                    {
+                        Kliked = false;
+                    }
+                    else
+                    {
+                        Kliked = true;
+                    }
+                }
+            }
+        }
+
+        if (Kliked)
+        {
+            transform.position = Vector3.Lerp(transform.position, MousePos, 0.98f);
+        }
+
+        
+	}
+}
