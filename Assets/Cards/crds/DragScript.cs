b8c807cd5d44617b20e492e0e6168c0fc784fb19
Geert Tullmans <519753@student.glu.nl>
2018-06-01 09:45:56 +0200

    DragScrip is af

--- Assets/Cards/crds/DragScript.cs
+++ Assets/Cards/crds/DragScript.cs
@@ -3,47 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragScript : MonoBehaviour {
-    private Vector3 MousePos;
-    private bool Kliked;
-    private Collider CardCollider;
+    private Vector3 _MousePos;
+    private bool _Kliked;
+    private Collider _CardCollider;
     // Use this for initialization
     void Start () {
-        Kliked = false;
-        CardCollider = gameObject.GetComponent<Collider>();
+        _Kliked = false;
+        _CardCollider = gameObject.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
-	void Update () { 
+	void Update () {
+        
+        _MousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
 
-        MousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+
+        DragAndDrop(_MousePos);
+        
+
+
+
+
+
+    }
+
+
+
+    private void DragAndDrop(Vector3 mousepos)
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
-            { 
-                
-                if (hit.collider == CardCollider)
+            {
+
+                if (hit.collider == _CardCollider)
                 {
 
 
-                    if (Kliked)
+
+                    _Kliked = true;
+
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _Kliked = false;
+        }
+
+        if (_Kliked)
+        {
+            transform.position = Vector3.Lerp(transform.position, _MousePos, 0.98f);
+        }
+    }
+
+    private void ClickToPick(Vector3 mousepos)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.collider == _CardCollider)
+                {
+
+
+                    if (_Kliked)
                     {
-                        Kliked = false;
+                        _Kliked = false;
                     }
                     else
                     {
-                        Kliked = true;
+                        _Kliked = true;
                     }
                 }
             }
         }
 
-        if (Kliked)
+        if (_Kliked)
         {
-            transform.position = Vector3.Lerp(transform.position, MousePos, 0.98f);
+            transform.position = Vector3.Lerp(transform.position, _MousePos, 0.98f);
         }
-
-        
-	}
+    }
 }
